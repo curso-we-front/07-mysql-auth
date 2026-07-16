018f3443e8c5c57f6e0437125d5513cc1ba696ec
jaimeveloso <jaimeveloso.cond@gmail.com>
2026-07-16 15:36:13 +0200

    [Added] users, endpoints and middleware

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const router = express.Router();
-const { validateArticle } = require('../middlewares/validate');
-const { getAll, getById, create, update, remove } = require('../controllers/articles');
+const express = require("express")
+const router = express.Router()
+const { validateArticle } = require("../middlewares/validate")
+const { getAll, getById, create, update, remove } = require("../controllers/articles")
 
-// Tarea 4: descomenta esta línea cuando hayas completado la Tarea 3
-// const { requireAuth } = require('../middlewares/auth');
+const { requireAuth } = require("../middlewares/auth")
 
-router.get('/', getAll);
-router.get('/:id', getById);
+router.get("/", getAll)
+router.get("/:id", getById)
+router.post("/", requireAuth, validateArticle, create)
+router.put("/:id", requireAuth, update)
+router.patch("/:id", requireAuth, update)
+router.delete("/:id", requireAuth, remove)
 
-// Tarea 4: añade requireAuth como middleware antes del handler en las rutas de escritura
-router.post('/', validateArticle, create);
-router.put('/:id', update);
-router.patch('/:id', update);
-router.delete('/:id', remove);
-
-module.exports = router;
+module.exports = router
